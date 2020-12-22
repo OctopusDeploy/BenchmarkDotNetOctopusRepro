916c786892f3286b1c3764c046b89bb465800dbc
Paul Gradie <paul.e.gradie@gmail.com>
2020-12-22 14:04:18 +1100

    even more so minimal as console project

---
+++ source/Demo.Project/DemoBenchmark.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using BenchmarkDotNet.Attributes;
+
+namespace Demo.Project
+{
+    public class DemoBenchmark
+    {
+        [Benchmark]
+        public void DemoBenchmarkMethod()
+        {
+            for (var index = 0; index < 10; index++) Thread.Sleep(500);
+        }
+    }
+}
---
+++ source/Demo.Project/Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+
+namespace Demo.Project
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            BenchmarkRunner.Run<DemoBenchmark>(new DebugBuildConfig());
+
+        }
+    }
+}
